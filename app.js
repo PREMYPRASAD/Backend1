7ccd4aa94b859c39e790b4d96685781c661a3ae7
PREMYPRASAD <mailtopremy2010@gmail.com>
2022-12-09 15:33:59 +0530

    changes

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
+const path = require('path');
+
 
 require('./middleware/mongoDB') // initiate mongodb connection
 
@@ -9,11 +11,14 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
-
+//static folder  
+app.use(express.static(path.resolve(__dirname, './uploads')));
 const PORT = process.env.PORT || 3000;
 
 const userApi = require('./routes/user');
 app.use('/api', userApi);
+const studentApi = require('./routes/student');
+app.use('/api', studentApi);
 
 //server code
 app.listen(PORT, () => {
